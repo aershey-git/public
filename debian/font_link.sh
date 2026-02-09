ac9d1879c8ecf994fd2eec540a785a11968d560c
aershey-git <79946517+aershey-git@users.noreply.github.com>
2026-02-09 15:26:20 -0500

    Create font_link.sh

---
+++ debian/font_link.sh
@@ -0,0 +1,32 @@
+sudo ln -s 'Courier Prime Sans.ttf' Andale_Mono.ttf 
+sudo ln -s 'Courier Prime.ttf' Courier_New.ttf
+sudo ln -s 'Courier Prime Bold.ttf' Courier_New_Bold.ttf
+sudo ln -s 'Courier Prime Italic.ttf' Courier_New_Italic.ttf
+sudo ln -s 'Courier Prime Bold Italic.ttf' Courier_New_Bold_Italic.ttf
+
+sudo ln -s ComicNeue-Regular.otf Comic_Sans_MS.ttf
+sudo ln -s ComicNeue-Bold.otf Comic_Sans_MS_Bold.ttf
+
+# Arial_Black.ttf
+sudo ln -s 'Arial Nova.ttf' Arial.ttf
+sudo ln -s 'Arial Nova Bold.ttf' Arial_Bold.ttf
+sudo ln -s 'Arial Nova Bold Italic.ttf' Arial_Bold_Italic.ttf
+sudo ln -s 'Arial Nova Italic.ttf' Arial_Italic.ttf
+sudo ln -s 'Georgia Pro.ttf' Georgia.ttf
+sudo ln -s 'Georgia Pro Bold.ttf' Georgia_Bold.ttf
+sudo ln -s 'Georgia Pro Italic.ttf' Georgia_Italic.ttf
+sudo ln -s 'Georgia Pro Bold Italic.ttf' Georgia_Bold_Italic.ttf
+# Impact.ttf
+# Trebuchet_MS.ttf
+# Trebuchet_MS_Bold.ttf
+# Trebuchet_MS_Italic.ttf
+# Trebuchet_MS_Bold_Italic.ttf
+sudo ln -s 'Verdana Pro.ttf' Verdana.ttf
+sudo ln -s 'Verdana Pro Bold.ttf' Verdana_Bold.ttf
+sudo ln -s 'Verdana Pro Italic.ttf' Verdana_Italic.ttf
+sudo ln -s 'Verdana Pro Bold Italic.ttf' Verdana_Bold_Italic.ttf
+# Webdings.ttf
+# sudo ln -s Times_New_Roman.ttf
+# sudo ln -s Times_New_Roman_Bold.ttf
+# sudo ln -s Times_New_Roman_Bold_Italic.ttf
+# sudo ln -s Times_New_Roman_Italic.ttf
