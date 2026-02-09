fa12bde0549260eee902be569727917a0e1eb204
aershey-git <79946517+aershey-git@users.noreply.github.com>
2026-02-09 15:26:35 -0500

    Update test_fonts.sh

--- debian/test_fonts.sh
+++ debian/test_fonts.sh
@@ -1,4 +1,73 @@
-for family in emoji serif sans-serif monospace cursive fantasy Arial Arimo courier Cousine times Tinos Calibri Carlito Cambria Caladea Candara Consolas Constantia Corbel Georgia Verdana Helvetica "Helvetica Neue" "Trebuchet MS" Webdings "Comic Sans MS" "Andale Mono" "Times New Roman" "Courier New" "open sans" "segoe ui" "apple-system" "BlinkMacSystemFont" "roboto" "helvetica neue" "Garamond" "Futura" "Frutiger" Tenorite Bierstadt Skeena Seaford Grandview; do
+for family in \
+emoji \
+serif \
+sans-serif \
+monospace \
+cursive \
+fantasy \
+Arial \
+Arimo \
+times \
+"Times New Roman" \
+Tinos \
+courier \
+"Courier New" \
+Cousine \
+Calibri \
+Carlito \
+Cambria \
+Caladea \
+Candara  \
+Consolas \
+Constantia \
+Corbel \
+Georgia \
+Verdana \
+Helvetica \
+"Helvetica Neue" \
+"apple-system" \
+"Adwaita Mono" \
+"Adwaita Sans" \
+"Andale Mono" \
+"Apple Garamond" \
+"avenir next" \
+"BlinkMacSystemFont" \
+"Brush Script MT" \
+"Comic Sans MS" \
+"Courier New" \
+"Droid Sans" \
+"Droid Serif" \
+"Fira Sans" \
+"Frutiger" \
+"Futura" \
+"Garamond" \
+"Liberation Mono" \
+"Lucida Console" \
+"Lucida Grande" \
+"Minion Pro" \
+"Myriad Pro" \
+"open sans" \
+"San Francisco" \
+"Segoe UI" \
+"Source Serif Pro" \
+"Trebuchet MS" \
+avenir \
+Baskerville \
+Cantarell \
+Franklin \
+Inter \
+Menlo \
+Aptos \
+Monaco \
+noto \
+Oxygen \
+Roboto \
+Tahoma \
+Ubuntu \
+Webdings \
+Impact ; do
 echo -n "$family: "
   fc-match "$family"
 done
+
+
